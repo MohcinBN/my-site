d3fe26596751f8eaf081528fb87a84f760a0b01c
MohcinBN <bounouara.mohcin@gmail.com>
2023-06-17 11:29:15 +0100

    change layout for about page.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,16 @@
-@extends('layouts.app')
-<div class="container mt-5 col-md-8 mx-auto">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Scripts -->
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        <title>Mohcin Bounouara</title>
+
+    </head>
+    <body class="bg-white">
+        <div class="container mt-5 col-md-8 mx-auto">
     @include('layouts.header')
     <div class="row">
         @include('layouts.nav')
@@ -8,4 +19,5 @@
                 test about 
         </div>
     </div>
+</div>
 </div>
