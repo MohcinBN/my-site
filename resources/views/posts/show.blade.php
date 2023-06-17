73365cbcfd8ee6ff3ae1deba2c943085b76415b8
MohcinBN <bounouara.mohcin@gmail.com>
2023-06-17 11:46:18 +0100

    Add google analytics

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,6 +8,18 @@
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
         <title>Mohcin Bounouara</title>
 
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KCBYVGJBPB"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KCBYVGJBPB');
+</script>
+
+
     </head>
     <body class="bg-white">
         <div class="container mt-5 col-md-8 mx-auto">
