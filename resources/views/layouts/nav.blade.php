04880c6206a1f125c14116d721c0e41b23766445
MohcinBN <bounouara.mohcin@gmail.com>
2023-06-19 21:48:28 +0100

    Add about page + nav social links + small css changes..

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -9,9 +9,9 @@
 
         <div class="border-gray-500 border-end  border-bottom mt-4">
             <ul class="pr-0 list-unstyled">
-                <li class="mb-2"><a href="" class="item fw-bold text-decoration-none text-black fs-5">LinkedIn</a></li>
-                <li class="mb-2"><a href="" class="item fw-bold text-decoration-none text-black fs-5">Github</a></li>
-                <li class="mb-2"><a href="" class="item fw-bold text-decoration-none text-black fs-5">Twitter</a></li>
+                <li class="mb-2"><a href="https://www.linkedin.com/in/mohcin-bounouara-31697b69/" target="_blank" class="item fw-bold text-decoration-none text-black fs-5">LinkedIn</a></li>
+                <li class="mb-2"><a href="https://github.com/MohcinBN" target="_blank" class="item fw-bold text-decoration-none text-black fs-5">Github</a></li>
+                <li class="mb-2"><a href="https://twitter.com/BN_Geek" target="_blank"  class="item fw-bold text-decoration-none text-black fs-5">Twitter</a></li>
             </ul>
         </div>
     </nav>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -28,7 +28,39 @@
         @include('layouts.nav')
         {{-- articles --}}
         <div class="col-md-9 mt-4">
-                test about 
+               <h1>About me</h1>
+               <p>
+                Muslim, software developer, striving for daily self-improvement in the field of software engineering. <br><br>
+
+                I have had the opportunity to work in various teams, and I hold great respect for them. During my search for a team that values mutual respect and treats individuals as human beings rather than mere production machines, also push you to discover new things in your career, I found the amazing <a href="https://contentcoffee.com/">Content & Coffee</a> company.  <br><br>
+                
+                I truly feel like a part of their family, and I strive to contribute my best to the team.  <br><br>
+
+                Apart from my professional pursuits, I am constantly working on personal growth, cultivating good habits, and eliminating bad ones. I aim to be a valuable asset to my family, offering assistance whenever possible situations.  <br><br>
+
+                In addition to software development, I have a keen interest in sharing knowledge and thoughts on various aspects of life. Therefore, I intend to use this blog as a platform to discuss a wide range of topics the main of them is software engineering ones.  <br><br> 
+
+                Technologies and tools I work with:   </p>
+
+                <p>
+                    <ul>
+                    <li>PHP</li>
+                    <li>Laravel</li>
+                    <li>JavaScript</li>
+                    <li>Linux</li>
+                    <li>CSS</li>
+                    <li>HTML</li>
+                    <li>SCSS</li>
+                    <li>Vue.js</li>
+                    <li>Testing tools and frameworks</li>
+                </ul>
+                </p>
+
+                
+                <p> I firmly believe in taking small and continuous steps in my life's journey..  <br><br>
+
+                ‘A’isha reported God’s Messenger as saying, “The acts most pleasing to God are those which are done most continuously, even if they amount to little.” (Bukhari and Muslim.)  <br><br>
+               </p>
         </div>
     </div>
 </div>
