cfc396a4659ebb305c4f11d84cc5314636badcc2
MohcinBN <bounouara.mohcin@gmail.com>
2023-06-30 12:09:12 +0100

    refactoring head section and make it more readable and maintainable

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- Scripts -->
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-        <title>Mohcin Bounouara</title>
-
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-KCBYVGJBPB"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-KCBYVGJBPB');
-</script>
-
-
-    </head>
-    <body class="bg-white">
+@include('layouts.head')
         <div class="container mt-5 col-md-8 mx-auto">
             @include('layouts.header')
             <div class="row">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- Scripts -->
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-        <title>Mohcin Bounouara</title>
-
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-KCBYVGJBPB"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-KCBYVGJBPB');
-</script>
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    </head>
-    <body class="bg-white">
+@include('layouts.head')
         <div class="container mt-5 col-md-8 mx-auto">
             @include('layouts.header')
             <div class="row">
