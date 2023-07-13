bda4be786d460d374af823915aadc51c0c607ba6
MohcinBN <bounouara.mohcin@gmail.com>
2023-07-13 14:53:29 +0200

    some changes in filament user access

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+use Filament\Models\Contracts\FilamentUser;
+
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
