86ec62792f2431f31b7f605addcb4011ef313bb7
MohcinBN <bounouara.mohcin@gmail.com>
2023-07-21 18:17:22 +0200

    Add tag resource with te ability to select tags for posts

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource\RelationManagers;
 use App\Models\Post;
+use App\Models\Tag;
 use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
@@ -37,6 +38,11 @@ class PostResource extends Resource
                 Forms\Components\FileUpload::make('image')
                     ->preserveFilenames()
                     ->maxSize(1024),
+
+                Forms\Components\Select::make('id')
+                    ->label('Tag')
+                    ->options(Tag::all()->pluck('name', 'id'))
+                    ->searchable()
             ]);
     }
 
