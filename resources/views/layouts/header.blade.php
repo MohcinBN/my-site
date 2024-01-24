27cbd9805c7fc0d6f9aa79146b83c2578e860daf
MohcinBN <bounouara.mohcin@gmail.com>
2024-01-24 21:06:24 +0100

    add pull option

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,6 +3,6 @@
         <h1>
             <a href="/" class="text-black text-decoration-none">Mohcin Bounouara</a>
         </h1>
-        <p class="">A Blog on software development and own thoughts -----</p>
+        <p class="">A Blog on software development and own thoughts</p>
     </header>
 </div>
